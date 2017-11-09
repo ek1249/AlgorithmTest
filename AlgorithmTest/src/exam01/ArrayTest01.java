48a82b08943e038b6551cc577ff8963d7653d9d3
jihye <ek1249@hanmail.net>
2017-11-09 18:12:24 +0900

    commit

--- AlgorithmTest/src/exam01/ArrayTest01.java
+++ AlgorithmTest/src/exam01/ArrayTest01.java
@@ -3,7 +3,7 @@ package exam01;
 import java.util.Scanner;
 
 public class ArrayTest01 {
-
+ 
 	/*영어 대소문자와 띄어쓰기만으로 이루어진 문장이 주어진다. 이 문장에는 몇 개의 단어가 있을까? 이를 구하는 프로그램을 작성하시오. 단, 단어는 띄어쓰기 하나로 구분된다고 생각한다.*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
